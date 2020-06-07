f626d18fe1e4c4a6d128372e58d0577c0696e9ac
kishanhitk <kishansharma1231@gmail.com>
2020-06-07 16:30:45 +0530

    Login working from React

--- scraper.js
+++ scraper.js
@@ -1,58 +1,21 @@
 const puppeteer = require("puppeteer");
 const cors = require("cors")({ origin: true });
 
-const scrapeImages = async () => {
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  // await page.goto("http://juadmission.jdvu.ac.in/jums_exam/");
-  // // console.log(await page.content());
-  // await page.screenshot({ path: "screenshot.png" });
-  // await page.type("[name=uname]", "001811601047");
-  // await page.type("[name=pass]", "158261ed");
-  // await page.click("[type=submit]");
-  // await page.waitFor(5000);
-  // await page.screenshot({ path: "screenshot.png" });
-  // const data = await page.evaluate(() => {
-  //   const name = document
-  //     .querySelector(
-  //       "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2)"
-  //     )
-  //     .textContent.trim();
-  //   const course = document
-  //     .querySelector(
-  //       "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2)"
-  //     )
-  //     .textContent.trim();
-  //   return {
-  //     name: name,
-  //     course: course,
-  //   };
-  // });
-  // str = JSON.stringify(data);
-  // console.log(str);
-  // await page.waitFor(5000);
-  // await page.goto(
-  //   "http://juadmission.jdvu.ac.in/jums_exam/student_odd_2019/index.jsp"
-  // );
-  // await page.waitFor(5000);
-  // await page.screenshot({ path: "screenshot.png", fullPage: true });
-  // await browser.close();
-};
-
-// scrapeImages();
-
 exports.getData = async (req, res) => {
   cors(req, res, async () => {
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
     await page.goto("http://juadmission.jdvu.ac.in/jums_exam/");
+    uname = req.body.uname;
 
     // console.log(await page.content());
     await page.screenshot({ path: "screenshot.png" });
-    await page.type("[name=uname]", "001811601047");
+    //ID 001811601047
+    await page.type("[name=uname]", req.body.uname);
 
-    await page.type("[name=pass]", "158261ed");
+    //PASSWORD 158261ed
+    await page.type("[name=pass]", req.body.pass);
 
     await page.click("[type=submit]");
     await page.waitFor(5000);
@@ -87,3 +50,5 @@ exports.getData = async (req, res) => {
     await browser.close();
   });
 };
+
+//PASSWORD 158261ed
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 const { getData } = require("./scraper");
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 1000;
 app.listen(port, console.log(`App is running on ${port}`));
-app.get("/", getData);
+// app.get("/", getData);
+app.post("/", getData);
