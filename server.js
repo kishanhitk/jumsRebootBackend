f7f67417b4c4aa4a542d0cf6267490064f38a3d1
kishanhitk <kishansharma1231@gmail.com>
2020-06-08 11:02:33 +0530

    Added server timeout

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 1000;
-app.listen(port, console.log(`App is running on ${port}`));
+app.listen(port, console.log(`App is running on ${port}`)).setTimeout(0);
 // app.get("/", getData);
 app.post("/", getData);
 app.get("/test", (req, res) => {
   res.send("Hello");
 });
 app.post("/semester", getSemesterData);
+
