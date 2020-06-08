4025248c18adb87e1e5978f941ed29ed69c7c279
kishanhitk <kishansharma1231@gmail.com>
2020-06-08 11:49:57 +0530

    Added proper logs and waitForSelector

--- scraper.js
+++ scraper.js
@@ -3,10 +3,12 @@ const cors = require("cors")({ origin: true });
 
 exports.getData = async (req, res) => {
   cors(req, res, async () => {
+    console.log("Starting Process");
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
     await page.goto("http://juadmission.jdvu.ac.in/jums_exam/");
+    console.log("Loaded Login Page");
     // uname = req.body.uname;
 
     // console.log(await page.content());
@@ -18,37 +20,50 @@ exports.getData = async (req, res) => {
     await page.type("[name=pass]", req.body.pass);
 
     await page.click("[type=submit]");
+    console.log("Filled Credentials");
     await page.waitFor(3000);
+    console.log("Loaded Profile Page");
     // await page.screenshot({ path: "screenshot.png" });
-    const data = await page.evaluate(() => {
-      const name = document
-        .querySelector(
-          "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2)"
-        )
-        .textContent.trim();
-      const course = document
-        .querySelector(
-          "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2)"
-        )
-        .textContent.trim();
-      const imgUrl = document.querySelector(
-        "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(2) > img"
-      );
-      const buttons = [];
-      document.querySelectorAll("#submit_button").forEach((a) => {
-        buttons.push({ text: a.textContent, link: a.href });
-        // console.log({ text: a.textContent, link: a.href });
+    var data = {};
+    await page.waitForSelector("table").then(async () => {
+      console.log("Populating data");
+      data = await page.evaluate(() => {
+        const name = document
+          .querySelector(
+            "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2)"
+          )
+          .textContent.trim();
+        console.log("Got name");
+        const course = document
+          .querySelector(
+            "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2)"
+          )
+          .textContent.trim();
+        console.log("Got Courses");
+        const imgUrl = document.querySelector(
+          "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > table > tbody > tr > td:nth-child(2) > img"
+        );
+        console.log("Got imgUrl");
+        const buttons = [];
+        document.querySelectorAll("#submit_button").forEach((a) => {
+          buttons.push({ text: a.textContent, link: a.href });
+          // console.log({ text: a.textContent, link: a.href });
+        });
+        // console.log(buttons);
+        console.log("Got Buttons");
+        return {
+          name: name,
+          course: course,
+          buttons: buttons,
+          imgUrl: imgUrl.src,
+        };
       });
-      // console.log(buttons);
-      return {
-        name: name,
-        course: course,
-        buttons: buttons,
-        imgUrl: imgUrl.src,
-      };
+      console.log(await data);
     });
+    console.log("Got Data from Profile Page");
     str = JSON.stringify(data);
     res.json(data);
+    console.log("Sent Response");
     // console.log(data.name);
     // await page.waitFor(1000);
 
@@ -59,6 +74,7 @@ exports.getData = async (req, res) => {
     // await page.waitFor(3000);
     // await page.screenshot({ path: "screenshot.png", fullPage: true });
     await browser.close();
+    console.log("Closed Browser");
   });
 };
 
