7964f5a9fd69bc7555ff521e91d92a79b787696f
kishanhitk <kishansharma1231@gmail.com>
2020-06-09 20:27:57 +0530

    Added notice and better console.logs.

--- scraper.js
+++ scraper.js
@@ -11,7 +11,17 @@ exports.getData = async (req, res) => {
     await page.goto("http://juadmission.jdvu.ac.in/jums_exam/");
     console.log("Loaded Login Page");
     // uname = req.body.uname;
-
+    var notice = "";
+    await page.waitForSelector("table").then(async () => {
+      notice = await page.evaluate(() => {
+        const notice = document
+          .querySelector("body > table > tbody > tr > td > font > div")
+          .innerText.trim()
+          .split("\n\n");
+        return notice;
+      });
+    });
+    console.log(notice);
     // console.log(await page.content());
     // await page.screenshot({ path: "screenshot.png" });
     //ID 001811601047
@@ -22,7 +32,7 @@ exports.getData = async (req, res) => {
 
     await page.click("[type=submit]");
     console.log("Filled Credentials");
-    await page.waitFor(3000);
+    await page.waitFor(1000);
     console.log("Loaded Profile Page");
     // await page.screenshot({ path: "screenshot.png" });
     var data = {};
@@ -51,6 +61,7 @@ exports.getData = async (req, res) => {
           // console.log({ text: a.textContent, link: a.href });
         });
         // console.log(buttons);
+
         console.log("Got Buttons");
         return {
           name: name,
@@ -59,10 +70,11 @@ exports.getData = async (req, res) => {
           imgUrl: imgUrl.src,
         };
       });
-      console.log(await data);
+      // console.log(await data);
     });
     console.log("Got Data from Profile Page");
     str = JSON.stringify(data);
+    data.notices = notice;
     res.json(data);
     console.log("Sent Response");
     // console.log(data.name);
@@ -81,9 +93,10 @@ exports.getData = async (req, res) => {
 
 //PASSWORD 158261ed
 exports.getSemesterData = async (req, res) => {
+  console.log("Started Process");
   cors(req, res, async () => {
     const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-
+    console.log("Launched Browser");
     const page = await browser.newPage();
     await page.goto("http://juadmission.jdvu.ac.in/jums_exam/");
     // uname = req.body.uname;
@@ -97,10 +110,13 @@ exports.getSemesterData = async (req, res) => {
     await page.type("[name=pass]", req.body.pass);
 
     await page.click("[type=submit]");
+    console.log("Filled Credentials");
     await page.waitFor(1000);
     // await page.screenshot({ path: "screenshot.png" });
     await page.goto(req.body.url);
+
     await page.waitFor(1000);
+    console.log("Go to Semester Page");
     // await page.screenshot({ path: "screenshot.png", fullPage: true });
     const data = await page.evaluate(() => {
       // const admitCard = document.querySelector(
@@ -109,22 +125,26 @@ exports.getSemesterData = async (req, res) => {
       const admitCard = document.querySelector(
         "body > div.easyui-layout.layout.easyui-fluid > div.panel.layout-panel.layout-panel-center > div.panel-body.layout-body > div > table > tbody > tr > td:nth-child(5) > a"
       ).href;
-
+      console.log("Got url of Grade Card");
       return {
         admitCard: admitCard,
       };
     });
     await page.goto(data.admitCard);
+    console.log("Goto Admit Card URL");
     // await page.waitFor(3000);
     // str = JSON.stringify(data);
     // console.log(await page.content());
     await page.emulateMediaType("screen");
     await page.pdf({ path: `${req.body.uname}.pdf` });
+    console.log("Coverted to PDF");
     res.download(`./${req.body.uname}.pdf`);
+    console.log("Converted to PDF");
     // console.log(data.admitCard);
     // await page.waitFor(3000);
 
     await browser.close();
+    console.log("Closed Browser");
     fs.unlink(`${req.body.uname}.pdf`, function (err) {
       if (err) {
         throw err;
